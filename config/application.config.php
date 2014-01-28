b605ecc5efed7b2c9d048ebfda9095534b7edaf0
renaatdemuynck <renaat.demuynck@gmail.com>
2014-01-28 13:51:34 +0100

    added override config files

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,9 @@ return array(
         'Application'
     ),
     'module_listener_options' => array(
+        'config_glob_paths' => array(
+            'config/autoload/{,*.}{global,local}.php'
+        ),
         'module_paths' => array(
             './module',
             './vendor'
