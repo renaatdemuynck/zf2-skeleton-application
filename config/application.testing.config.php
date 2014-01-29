2cda3562d8cdc60647094082e77a98567f7862e4
renaatdemuynck <renaat.demuynck@gmail.com>
2014-01-29 13:27:53 +0100

    added caching options

--- config/application.testing.config.php
+++ config/application.testing.config.php
@@ -1,3 +1,9 @@
 <?php
 
-return array();
+return array(
+    'module_listener_options' => array(
+        'config_cache_enabled' => true,
+        'module_map_cache_enabled' => true,
+        'cache_dir' => 'data/cache/'
+    )
+);
