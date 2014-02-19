56505b5af13278d5517d7f51679ed02e0786ca15
renaatdemuynck <renaat.demuynck@gmail.com>
2014-02-19 15:36:27 +0100

    added support for autoloading module config files

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -6,7 +6,14 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        $config = include __DIR__ . '/config/module.config.php';
+        $files = glob(__DIR__ . '/config/autoload/*.config.php');
+        
+        foreach ($files as $file) {
+            $config = array_merge_recursive($config, include $file);
+        }
+        
+        return $config;
     }
 
     public function getAutoloaderConfig()
